6af60fe763cefe2cfb393f08b77120d4d38a88e5
competitor3 <gavriel228@gmail.com>
2022-07-13 20:47:31 +0700

    Add project files.

---
+++ MacUI/MacUIForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace MacUI
+{
+    partial class MacUIForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnFullscreen = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.btnMinimize = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.timerMinimize = new System.Windows.Forms.Timer(this.components);
+            this.timerNormal = new System.Windows.Forms.Timer(this.components);
+            this.timerMaximize = new System.Windows.Forms.Timer(this.components);
+            this.panelTitleBar.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFullscreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.White;
+            this.panelTitleBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTitleBar.Controls.Add(this.tableLayoutPanel1);
+            this.panelTitleBar.Controls.Add(this.lblTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Padding = new System.Windows.Forms.Padding(8);
+            this.panelTitleBar.Size = new System.Drawing.Size(800, 40);
+            this.panelTitleBar.TabIndex = 2;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+            this.panelTitleBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseMove);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btnFullscreen, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnClose, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnMinimize, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(66, 22);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnFullscreen
+            // 
+            this.btnFullscreen.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnFullscreen.Location = new System.Drawing.Point(47, 3);
+            this.btnFullscreen.Name = "btnFullscreen";
+            this.btnFullscreen.Size = new System.Drawing.Size(16, 16);
+            this.btnFullscreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnFullscreen.TabIndex = 2;
+            this.btnFullscreen.TabStop = false;
+            this.btnFullscreen.Click += new System.EventHandler(this.btnFullscreen_Click);
+            this.btnFullscreen.MouseEnter += new System.EventHandler(this.btnFullscreen_MouseEnter);
+            this.btnFullscreen.MouseLeave += new System.EventHandler(this.btnFullscreen_MouseLeave);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnClose.Location = new System.Drawing.Point(3, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(16, 16);
+            this.btnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnClose.TabIndex = 0;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.btnClose.MouseEnter += new System.EventHandler(this.btnClose_MouseEnter);
+            this.btnClose.MouseLeave += new System.EventHandler(this.btnClose_MouseLeave);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnMinimize.Location = new System.Drawing.Point(25, 3);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(16, 16);
+            this.btnMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.TabStop = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            this.btnMinimize.MouseEnter += new System.EventHandler(this.btnMinimize_MouseEnter);
+            this.btnMinimize.MouseLeave += new System.EventHandler(this.btnMinimize_MouseLeave);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitle.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(8, 8);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(782, 22);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "TitleLabel";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseDown);
+            this.lblTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblTitle_MouseMove);
+            // 
+            // timerMinimize
+            // 
+            this.timerMinimize.Interval = 1;
+            this.timerMinimize.Tick += new System.EventHandler(this.timerMinimize_Tick);
+            // 
+            // timerNormal
+            // 
+            this.timerNormal.Interval = 1;
+            this.timerNormal.Tick += new System.EventHandler(this.timerNormal_Tick);
+            // 
+            // timerMaximize
+            // 
+            this.timerMaximize.Interval = 1;
+            this.timerMaximize.Tick += new System.EventHandler(this.timerMaximize_Tick);
+            // 
+            // MacUIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.panelTitleBar);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MacUIForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MacUI Library Form";
+            this.Load += new System.EventHandler(this.MacUIForm_Load);
+            this.Resize += new System.EventHandler(this.MacUIForm_Resize);
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFullscreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTitleBar;
+        protected System.Windows.Forms.Label lblTitle;
+        protected System.Windows.Forms.PictureBox btnClose;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timerMinimize;
+        private System.Windows.Forms.Timer timerNormal;
+        private System.Windows.Forms.Timer timerMaximize;
+        protected System.Windows.Forms.PictureBox btnFullscreen;
+        protected System.Windows.Forms.PictureBox btnMinimize;
+    }
+}
+
---
+++ MacUI/MacUIForm.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+
+namespace MacUI
+{
+    public partial class MacUIForm : Form
+    {
+        private int beforeX;
+        private int beforeY;
+        private string windowStatus = "Normal";
+        private int currentWidth = 800;
+        private int currentHeight = 600;
+        private int currentX;
+        private int currentY;
+
+        private Color redBackground = Color.FromArgb(255, 96, 92);
+        private Color redBorder = Color.FromArgb(233, 76, 70);
+        private Color redHoverContent = Color.FromArgb(92, 5, 5);
+        private Color yellowBackground = Color.FromArgb(255, 189, 68);
+        private Color yellowBorder = Color.FromArgb(232, 169, 37);
+        private Color yellowHoverContent = Color.FromArgb(153, 87, 0);
+        private Color greenBackground = Color.FromArgb(0, 202, 78);
+        private Color greenBorder = Color.FromArgb(36, 182, 54);
+        private Color greenHoverContent = Color.FromArgb(0, 100, 0);
+
+        public MacUIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MacUIForm_Load(object sender, EventArgs e)
+        {
+            DrawButton(btnClose, redBackground, redBorder);
+            DrawButton(btnMinimize, yellowBackground, yellowBorder);
+            DrawButton(btnFullscreen, greenBackground, greenBorder);
+
+            this.MaximizedBounds = Screen.GetWorkingArea(this);
+        }
+
+        private void DrawButton(PictureBox picBox, Color color, Color colorBorder)
+        {
+            int size = 14;
+            Bitmap btn = new Bitmap(size + 4, size + 4);
+
+            Graphics canvas = Graphics.FromImage(btn);
+            canvas.SmoothingMode = SmoothingMode.AntiAlias;
+            canvas.FillEllipse(new SolidBrush(color), 0, 0, size, size);
+            canvas.DrawEllipse(new Pen(colorBorder), 0, 0, size, size);
+
+            picBox.Image = btn;
+
+            canvas.Dispose();
+        }
+
+        private void DrawButtonHoverState(PictureBox picBox, Color color, Color colorBorder, Color colorHoverContent, string buttonType)
+        {
+            int size = 14;
+            Bitmap btn = new Bitmap(size + 4, size + 4);
+
+            Graphics canvas = Graphics.FromImage(btn);
+            canvas.SmoothingMode = SmoothingMode.AntiAlias;
+            canvas.FillEllipse(new SolidBrush(color), 0, 0, size, size);
+            canvas.DrawEllipse(new Pen(colorBorder), 0, 0, size, size);
+
+            if (buttonType == "Close")
+            {
+                canvas.DrawLine(new Pen(colorHoverContent), 4, 4, size - 4, size - 4);
+                canvas.DrawLine(new Pen(colorHoverContent), size - 4, 4, 4, size - 4);
+            }
+            else if (buttonType == "Minimize")
+            {
+                canvas.DrawLine(new Pen(colorHoverContent), 4, size / 2, size - 4, size / 2);
+            }
+            else
+            {
+                canvas.FillRectangle(new SolidBrush(colorHoverContent), 4, 4, size - 8, size - 8);
+                canvas.DrawLine(new Pen(color), 5, 4, size - 4, size - 5);
+                canvas.DrawLine(new Pen(color), 4, 4, size - 4, size - 4);
+                canvas.DrawLine(new Pen(color), 3, 4, size - 4, size - 3);
+            }
+
+            picBox.Image = btn;
+
+            canvas.Dispose();
+        }
+
+        private void lblTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(this.Location.X + (e.X - beforeX), this.Location.Y + (e.Y - beforeY));
+            }
+        }
+
+        private void lblTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                beforeX = e.X;
+                beforeY = e.Y;
+            }
+        }
+
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                beforeX = e.X;
+                beforeY = e.Y;
+            }
+        }
+
+        private void panelTitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(this.Location.X + (e.X - beforeX), this.Location.Y + (e.Y - beforeY));
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            if (windowStatus != "Maximize")
+            {
+                currentWidth = this.Size.Width;
+                currentHeight = this.Size.Height;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.Size = new Size(currentWidth, currentHeight);
+                this.Location = new Point(currentX, currentY);
+            }
+
+            timerMinimize.Start();
+        }
+
+        private void btnFullscreen_Click(object sender, EventArgs e)
+        {
+            if (windowStatus == "Maximize")
+            {
+                this.WindowState = FormWindowState.Normal;
+                timerNormal.Start();
+            }
+            else
+            {
+                currentWidth = this.Size.Width;
+                currentHeight = this.Size.Height;
+                currentX = this.Location.X;
+                currentY = this.Location.Y;
+
+                timerMaximize.Start();
+            }
+        }
+
+        private void MacUIForm_Resize(object sender, EventArgs e)
+        {
+            if (windowStatus == "Minimize")
+            {
+                if (this.WindowState != FormWindowState.Minimized)
+                {
+                    // Restore after minimize (click app icon on taskbar)
+                    windowStatus = "Normal";
+                    timerNormal.Start();
+                }
+            }
+            else if (windowStatus == "MinimizeFromMaximize")
+            {
+                if (this.WindowState != FormWindowState.Minimized)
+                {
+                    // Restore after minimize (click app icon on taskbar)
+                    timerMaximize.Start();
+                }
+            }
+        }
+
+        private void timerMinimize_Tick(object sender, EventArgs e)
+        {
+            if (this.Size.Height >= 50 && this.Size.Width >= 100)
+            {
+                int reduction = 20;
+
+                this.Size = new Size(this.Size.Width - reduction, this.Size.Height - reduction);
+                this.Location = new Point(this.Location.X + (reduction / 2), this.Location.Y + reduction);
+
+                if (this.Opacity > 0)
+                {
+                    this.Opacity -= 0.1;
+                }
+            }
+            else
+            {
+                timerMinimize.Stop();
+
+
+                if (windowStatus == "Maximize")
+                {
+                    windowStatus = "MinimizeFromMaximize";
+                }
+                else
+                {
+                    windowStatus = "Minimize";
+                }
+
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void timerNormal_Tick(object sender, EventArgs e)
+        {
+            if (windowStatus == "Maximize")
+            {
+                if (this.Size.Height > currentHeight && this.Size.Width > currentWidth)
+                {
+                    int addition = 20;
+
+                    this.Size = new Size(this.Size.Width - addition, this.Size.Height - addition);
+                    this.Location = new Point(this.Location.X + (addition / 2), this.Location.Y + (addition / 2));
+                }
+                else
+                {
+                    timerNormal.Stop();
+                    windowStatus = "Normal";
+                }
+            }
+            else
+            {
+                if (this.Size.Height < currentHeight && this.Size.Width < currentWidth)
+                {
+                    int addition = 20;
+
+                    this.Size = new Size(this.Size.Width + addition, this.Size.Height + addition);
+                    this.Location = new Point(this.Location.X - (addition / 2), this.Location.Y - addition);
+
+                    if (this.Opacity < 1)
+                    {
+                        this.Opacity += 0.1;
+                    }
+                }
+                else
+                {
+                    timerNormal.Stop();
+                    windowStatus = "Normal";
+                }
+            }
+        }
+
+        private void timerMaximize_Tick(object sender, EventArgs e)
+        {
+            if (this.Size.Height < this.MaximizedBounds.Height && this.Size.Width < this.MaximizedBounds.Width)
+            {
+                int addition = 20;
+
+                this.Size = new Size(this.Size.Width + addition, this.Size.Height + addition);
+                if (windowStatus == "MinimizeFromMaximize")
+                {
+                    this.Location = new Point(this.Location.X - (addition / 2), this.Location.Y - addition);
+                }
+                else
+                {
+                    this.Location = new Point(this.Location.X - (addition / 2), this.Location.Y - (addition / 2));
+                }
+
+                if (this.Opacity < 1)
+                {
+                    this.Opacity += 0.1;
+                }
+            }
+            else
+            {
+                timerMaximize.Stop();
+                windowStatus = "Maximize";
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void btnClose_MouseLeave(object sender, EventArgs e)
+        {
+            DrawButton(btnClose, redBackground, redBorder);
+        }
+
+        private void btnClose_MouseEnter(object sender, EventArgs e)
+        {
+            DrawButtonHoverState(btnClose, redBackground, redBorder, redHoverContent, "Close");
+        }
+
+        private void btnMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            DrawButtonHoverState(btnMinimize, yellowBackground, yellowBorder, yellowHoverContent, "Minimize");
+        }
+
+        private void btnMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            DrawButton(btnMinimize, yellowBackground, yellowBorder);
+        }
+
+        private void btnFullscreen_MouseEnter(object sender, EventArgs e)
+        {
+            DrawButtonHoverState(btnFullscreen, greenBackground, greenBorder, greenHoverContent, "Maximize");
+        }
+
+        private void btnFullscreen_MouseLeave(object sender, EventArgs e)
+        {
+            DrawButton(btnFullscreen, greenBackground, greenBorder);
+        }
+    }
+}
